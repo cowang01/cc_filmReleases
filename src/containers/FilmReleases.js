ac838664462eb353bd578e2569110a1a78cc1e3b
cowang01 <g_cowan27@hotmail.com>
2019-08-05 14:36:39 +0100

    set-up

---
+++ src/containers/FilmReleases.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Films from '../components/Films';
+
+class FilmReleases extends Component {
+
+render() {
+  return(
+      <>
+      <p>TestFilmReleases</p>
+      <Films />
+      </>
+    )
+  }
+}
+export default FilmReleases;
